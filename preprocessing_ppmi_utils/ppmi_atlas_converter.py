6de6f027027fe01c70427d1dff00eaf6c5b94190
yanismiraoui <yanis.miraoui19@imperial.ac.uk>
2023-11-15 11:42:36 -0800

    preprocessing working for ppmi

--- preprocessing_ppmi_utils/ppmi_atlas_converter.py
+++ preprocessing_ppmi_utils/ppmi_atlas_converter.py
@@ -4,6 +4,7 @@ import nilearn
 import os
 from nilearn import datasets
 from nilearn.maskers import NiftiLabelsMasker
+from nilearn.connectome import ConnectivityMeasure
 
 DATA_DIR = '../PPMI'
 
@@ -18,7 +19,7 @@ def get_nifti_files(path):
 
 nifti_files = get_nifti_files(DATA_DIR)
 # Sample only 10% of the files for testing purposes
-nifti_files = nifti_files[:int(len(nifti_files)/10)]
+# nifti_files = nifti_files[:int(len(nifti_files)/10)]
 
 
 shape_saver = {}
@@ -36,7 +37,7 @@ for nifti_path_dcm2niix in nifti_files:
             memory="nilearn_cache",
             verbose=5,
             labels=labels,
-            resampling_target="data",
+            resampling_target="labels",
         )
 
         time_series = masker.fit_transform(nifti_path_dcm2niix)
@@ -62,10 +63,18 @@ for nifti_path_dcm2niix in nifti_files:
         # Concatenate the two time series together
         time_series_concat = np.concatenate((time_series, time_series_sub), axis=1)
         concat_shape = time_series_concat.shape
+
+        # Compute correlation matrix
+        correlation_measure = ConnectivityMeasure(
+            kind="correlation",
+            standardize="zscore_sample",
+        )
+        correlation_matrix = correlation_measure.fit_transform([time_series_concat])[0]
+        correlation_matrix_shape = correlation_matrix.shape
         # Save the shapes
-        shape_saver[nifti_path_dcm2niix] = [cortical_shape, subcortical_shape, concat_shape]
+        shape_saver[nifti_path_dcm2niix] = [cortical_shape, subcortical_shape, concat_shape, correlation_matrix_shape]
     except Exception as e:
-        shape_saver[nifti_path_dcm2niix] = [None, None, None]
+        shape_saver[nifti_path_dcm2niix] = [None, None, None, None]
         print(e)
 
 # Save the shapes to a csv
